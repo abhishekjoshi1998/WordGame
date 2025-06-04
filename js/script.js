02a7e24befd392cfba007686bde8ed741b1c99cc
abhishekjoshi1998 <abhishekjoshi9997@gmail.com>
2025-06-04 23:22:56 +0530

    script

--- js/script.js
+++ js/script.js
@@ -34,6 +34,7 @@ const initGame = () => {
     inputField.setAttribute("maxlength", correctWord.length);
 }
 initGame();
+
 const checkWord = () => {
     let userWord = inputField.value.toLowerCase();
     if(!userWord) return alert("Please enter the word to check!");
